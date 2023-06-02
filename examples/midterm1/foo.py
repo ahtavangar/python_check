3cf1fdda094d016d419f4c2a742ca9deba033b69
Amir Tavangar <53286415+ahtavangar@users.noreply.github.com>
2023-06-01 20:26:45 -0400

    Update foo.py

--- examples/midterm1/foo.py
+++ examples/midterm1/foo.py
@@ -6,11 +6,15 @@ import os
 def file_exists_check():
     """Check if the 'midterm1.py' file exists"""
     if not os.path.isfile("midterm1.py"):
-        raise Failure("The 'midterm1.py' file does not exist")
+        raise check50.Failure("The 'midterm1.py' file does not exist")
 
 @check50.check(file_exists_check)
 def stats_check():
     """Check the output of the stats(l) function"""
+    
+    if not check50.exists("midterm1.py"):
+        return
+        
     # Define a sample list to test the stats(l) function
     sample_list = [1, 2, 3, 4, 5]
 
@@ -19,7 +23,7 @@ def stats_check():
         import midterm1
         stats_output = midterm1.stats(sample_list)
     except Exception as e:
-        raise Failure(f"Error while calling stats(l) function: {e}")
+        raise check50.Failure(f"Error while calling stats(l) function: {e}")
 
     # Retrieve the expected values using the statistics module
     expected_mean = statistics.mean(sample_list)
@@ -29,6 +33,6 @@ def stats_check():
 
     # Compare the expected values with the stats(l) output
     if stats_output != f"Mean: {expected_mean} \nMedian: {expected_median}\nStandard deviation: {expected_stdv} \nRange: {expected_range}":
-        raise Failure("The stats(l) function does not produce the correct output")
+        raise check50.Failure("The stats(l) function does not produce the correct output")
     print("Your code passed the stats_check!")
 
