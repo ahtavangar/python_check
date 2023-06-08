628fed86301b4ae83f150e93d5de3a0b56bf4282
Amir Tavangar <53286415+ahtavangar@users.noreply.github.com>
2023-06-08 19:58:02 -0400

    add find_mode function

--- examples/midterm1/foo.py
+++ examples/midterm1/foo.py
@@ -2,7 +2,17 @@ import check50
 import statistics
 import os
 import re
+import numpy as np
 
+def find_mode(data):
+    myList = [float(i) for i in data.split(",")]
+    mode = statistics.mode(myList)
+
+    # Check if there is a unique mode
+    if np.unique(myList, return_counts=True)[1].max() == 1:
+        return float("nan")
+    
+    return mode
 
 @check50.check()
 def file_exists_check():
@@ -10,6 +20,26 @@ def file_exists_check():
     if not os.path.isfile("midterm1test.py"):
         raise check50.Failure("The 'midterm1test.py' file does not exist")
 
+@check50.check(file_exists_check)
+def mode_check():
+    """Check the output of the find_mode(l) function"""
+    
+    # Define a sample list to test the stats(l) function
+    sample_list0 = '2, 2, 3, 4, 5'
+
+   
+    actual = check50.run("python3 midterm1test.py").stdin(sample_list0).stdout()
+    # Retrieve the expected values using the statistics module
+    sample_list0 = [2.0,2.0,3.0,4.0,5.0]
+        
+    expected = f"Mode: {find_mode(sample_list0}"
+    
+    # Compare the expected values with the stats(l) output
+    if not re.search(expected, actual):
+        help = "The find_mode(l) function does not produce the correct output"
+        raise check50.Mismatch(expected, actual, help=help)
+        #raise check50.Failure("The stats(l) function does not produce the correct output")
+        
 @check50.check(file_exists_check)
 def stats_check():
     """Check the output of the stats(l) function"""
@@ -41,5 +71,3 @@ def stats_check():
         raise check50.Mismatch(expected, actual, help=help)
         #raise check50.Failure("The stats(l) function does not produce the correct output")
         
-    print("Your code passed the stats_check!")
-
