5f5cc9386423a6fb4914d761615096de46bf2351
Amir Tavangar <53286415+ahtavangar@users.noreply.github.com>
2023-06-10 13:52:17 -0400

    Add extra help for Failure

--- examples/midterm2/foo.py
+++ examples/midterm2/foo.py
@@ -66,9 +66,10 @@ def extract_values_check2():
     expected = "{'Revenue': 2345678.0, 'Expenses': 1876543.0, 'Net Profit': 469135.0, 'Units Sold': 15000.0, 'Average Unit Price': 156.42}"
         
     if not re.search(expected, actual):
-        help = "extract_values() function does not produce the correct output"
-        raise check50.Mismatch(expected, actual, help=help)
-        #raise check50.Failure("The stats(l) function does not produce the correct output")
+        #help = "extract_values() function does not produce the correct output"
+        #raise check50.Mismatch(expected, actual, help=help)
+        help = "Make sure the financials for Location2 are in a dictionary and all values are float"
+        raise check50.Failure("extract_values() function does not produce the correct output", help = help)
         
 @check50.check(file_exists_check)
 def extract_values_check3():
@@ -98,6 +99,7 @@ def extract_values_check4():
     expected = "{'Revenue': 6789012.0, 'Expenses': 5012345.0, 'Net Profit': 1776667.0, 'Units Sold': 18000.0, 'Average Unit Price': 398.33}"
         
     if not re.search(expected, actual):
-        help = "extract_values() function does not produce the correct output"
-        raise check50.Mismatch(expected, actual, help=help)
-        #raise check50.Failure("The stats(l) function does not produce the correct output")
+        #help = "extract_values() function does not produce the correct output"
+        #raise check50.Mismatch(expected, actual, help=help)
+        help = "Make sure the financials for Location2 are in a dictionary and all values are float"
+        raise check50.Failure("extract_values() function does not produce the correct output", help = help)
