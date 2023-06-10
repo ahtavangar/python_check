d551b8a953526f79d602f6f21b89791a0885e616
Amir Tavangar <53286415+ahtavangar@users.noreply.github.com>
2023-06-09 20:39:20 -0400

    Update foo.py

--- examples/midterm2/foo.py
+++ examples/midterm2/foo.py
@@ -20,7 +20,7 @@ def extract_values_check1():
 
     actual = check50.run("python3 Assignment_2.py").stdin('sample.txt').stdout()
       
-    expected = {'Location1': {'Revenue': 1234567.0, 'Expenses': 987654.0, 'Net Profit': 246913.0, 'Units Sold': 10000.0, 'Average Unit Price': 123.45}, 'Location2': {'Revenue': 2345678.0, 'Expenses': 1876543.0, 'Net Profit': 469135.0, 'Units Sold': 15000.0, 'Average Unit Price': 156.42}}
+    expected = "{'Location1': {'Revenue': 1234567.0, 'Expenses': 987654.0, 'Net Profit': 246913.0, 'Units Sold': 10000.0, 'Average Unit Price': 123.45}, 'Location2': {'Revenue': 2345678.0, 'Expenses': 1876543.0, 'Net Profit': 469135.0, 'Units Sold': 15000.0, 'Average Unit Price': 156.42}}"
     #display version of expected to show when error raised
     #expected_dis = f"Mean: {ex_mean}\nMedian: {ex_median}\nMode: {ex_mode}\nStandard deviation: {ex_stdv}\nRange: {ex_range}"
     
